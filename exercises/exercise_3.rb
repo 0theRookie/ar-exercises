e8e1cdd6021dff8a4e505838a4d53f3a50ed2f93
Keishon Ross <keishonross97@gmail.com>
2018-12-04 20:30:23 +0000

    initial commit with first 3 exercises finished

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,10 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+puts "#{Store.count} stores added."
+
+@store3 = Store.find_by(id: 3)
+puts "@store3 is #{@store3.name}"
+@store3.destroy
+
+puts "#{Store.count} stores added."
