6039c4c8a1bef953bb8dea1c5f059f715484f4a7
Keishon Ross <keishonross97@gmail.com>
2018-12-05 01:17:01 +0000

    adds name and rate validations for all instances of the employee class

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,11 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
-@store1.employees.create(first_name: 'Robert', last_name: 'Connoryson', hourly_rate: 30)
+@store1.employees.create(first_name: 'Bubbilz', last_name: 'Connoryson', hourly_rate: 41)
 @store1.employees.create(first_name: 'Bill', last_name: 'Blakeson', hourly_rate: 50)
 @store1.employees.create(first_name: 'Jonah', last_name: 'Jameson', hourly_rate: 120)
 
-@store2.employees.create(first_name: 'Jim', last_name: 'Flemmingwayson', hourly_rate: 35)
+@store2.employees.create(first_name: 'Jim', last_name: 'Flemmingwayson', hourly_rate: 45)
 @store2.employees.create(first_name: 'Cam', last_name: 'Cumberbatchson', hourly_rate: 60)
 
 puts "Store 1 Employees : #{@store1.employees.count}"
